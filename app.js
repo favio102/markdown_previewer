9b1b2275a01127ef21b8224eeda3581b775e0c9b
favio102 <favio102@protonmail.com>
2023-03-30 15:45:24 +0900

    markdown project freecode camp

---
+++ app.js
@@ -0,0 +1,13 @@
+// const preview = document.getElementById("preview");
+// const editor = document.getElementById("editor");
+
+
+
+// // function updatePreview() {
+// //     preview.innerHTML = marked(editor);
+// // }
+
+
+// editor.addEventListener('input', function() {
+//     preview.innerHTML = editor.value;
+//   });
